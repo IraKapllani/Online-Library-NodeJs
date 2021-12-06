2be3a4dfab92357db01baf2f0b3583700fac9201
IraKapllani <79216566+IraKapllani@users.noreply.github.com>
2021-12-06 23:32:56 +0100

    updates

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const http = require('http');
 
 
-const routes = require('./routes');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log('In the middleware');
+});
+
+const server =  http.createServer(app);
 
 
-const server =  http.createServer(routes);
-
-
-server.listen(3001);
+server.listen(3000);
