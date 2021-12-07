d94e47b1fb22974c0f63c363b66eda633c0dcc98
IraKapllani <79216566+IraKapllani@users.noreply.github.com>
2021-12-07 07:30:55 +0100

    changes

--- server/server.js
+++ server/server.js
@@ -2,11 +2,23 @@ const http = require('http');
 
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('In the middleware');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use( '/add-books', (req, res, next)=>{
+    res.send('<form action="/books" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use( '/books', (req, res, next)=>{
+    console.log(req.body);
+   res.redirect('/');
+});
+
+app.use( '/', (req, res, next)=>{
+    res.send("<h1>Hello</h1>");
 });
 
 const server =  http.createServer(app);
