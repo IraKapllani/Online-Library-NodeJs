e3c858b5bf6609aabf1f0b89a544729d39b0b811
IraKapllani <79216566+IraKapllani@users.noreply.github.com>
2021-12-21 00:12:55 +0100

    fixes

--- server/app.js
+++ server/app.js
@@ -88,6 +88,16 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 //     next();
 // });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 
 app.use((error, req, res, next) => {
   console.log(error);
